24a12d43c0ae88085c50ece03a1e94b64a439451
TeaFant <tea.fantela@gmail.com>
2021-09-23 13:44:26 +0200

    counter

---
+++ src/components/Counters.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import Counter from './Counter';
+
+class Counters extends Component {
+    render() {
+        const { onReset, counters, onIncrement, onDecrement, onDelete } = this.props;
+
+        return(
+        <div>
+            <button 
+                onClick={onReset}
+                className="btn btn-primary btn-sm m-2">Reset</button>
+            {counters.map(counter=> 
+                <Counter key={counter.id} 
+                         onIncrement={onIncrement}
+                         onDecrement={onDecrement}
+                         onDelete={onDelete}
+                         counter={counter}
+                />)}             
+        </div>
+        );
+    }
+}
+
+export default Counters;
